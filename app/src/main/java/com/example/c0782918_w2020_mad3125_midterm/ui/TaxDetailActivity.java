6d9112113ca505b41aceb790eab6d48230937f56
Amanpreet Kaur <amankaur2628@gmail.com>
2020-04-08 20:27:19 -0300

    [Aman]TaxDetailActivity created

---
+++ app/src/main/java/com/example/c0782918_w2020_mad3125_midterm/ui/TaxDetailActivity.java
@@ -0,0 +1,16 @@
+package com.example.c0782918_w2020_mad3125_midterm.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.c0782918_w2020_mad3125_midterm.R;
+
+public class TaxDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tax_detail);
+    }
+}
