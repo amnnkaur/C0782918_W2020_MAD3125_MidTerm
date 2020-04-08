8b083f0c8de5eeb644e0664fc7839addff975a2d
Amanpreet Kaur <amankaur2628@gmail.com>
2020-04-08 01:59:15 -0300

    [Aman]CRACustomer(): Constructors and Getter-Setter methods added

--- app/src/main/java/com/example/c0782918_w2020_mad3125_midterm/model/CRACustomer.java
+++ app/src/main/java/com/example/c0782918_w2020_mad3125_midterm/model/CRACustomer.java
@@ -21,4 +21,166 @@ public class CRACustomer
     public double carryForwardRRSP;
     public double totalTaxableIncome;
     public double totalTaxPayed;
+
+    public CRACustomer() {}
+
+    public CRACustomer(long personSINNumber, String firstName, String lastName, String fullName,
+                       Date birthDate, String gender, int age, Date taxFilingDate,
+                       double grossIncome, double federalTax, double provincialTax, double cpp, double ei,
+                       double rrspContributed, double carryForwardRRSP, double totalTaxableIncome, double totalTaxPayed)
+    {
+        this.personSINNumber = personSINNumber;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.fullName = fullName;
+        this.birthDate = birthDate;
+        this.gender = gender;
+        this.age = age;
+        this.taxFilingDate = taxFilingDate;
+        this.grossIncome = grossIncome;
+        this.federalTax = federalTax;
+        this.provincialTax = provincialTax;
+        this.cpp = cpp;
+        this.ei = ei;
+        this.rrspContributed = rrspContributed;
+        this.carryForwardRRSP = carryForwardRRSP;
+        this.totalTaxableIncome = totalTaxableIncome;
+        this.totalTaxPayed = totalTaxPayed;
+    }
+
+    public long getPersonSINNumber() {
+        return personSINNumber;
+    }
+
+    public void setPersonSINNumber(long personSINNumber) {
+        this.personSINNumber = personSINNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Date getTaxFilingDate() {
+        return taxFilingDate;
+    }
+
+    public void setTaxFilingDate(Date taxFilingDate) {
+        this.taxFilingDate = taxFilingDate;
+    }
+
+    public double getGrossIncome() {
+        return grossIncome;
+    }
+
+    public void setGrossIncome(double grossIncome) {
+        this.grossIncome = grossIncome;
+    }
+
+    public double getFederalTax() {
+        return federalTax;
+    }
+
+    public void setFederalTax(double federalTax) {
+        this.federalTax = federalTax;
+    }
+
+    public double getProvincialTax() {
+        return provincialTax;
+    }
+
+    public void setProvincialTax(double provincialTax) {
+        this.provincialTax = provincialTax;
+    }
+
+    public double getCpp() {
+        return cpp;
+    }
+
+    public void setCpp(double cpp) {
+        this.cpp = cpp;
+    }
+
+    public double getEi() {
+        return ei;
+    }
+
+    public void setEi(double ei) {
+        this.ei = ei;
+    }
+
+    public double getRrspContributed() {
+        return rrspContributed;
+    }
+
+    public void setRrspContributed(double rrspContributed) {
+        this.rrspContributed = rrspContributed;
+    }
+
+    public double getCarryForwardRRSP() {
+        return carryForwardRRSP;
+    }
+
+    public void setCarryForwardRRSP(double carryForwardRRSP) {
+        this.carryForwardRRSP = carryForwardRRSP;
+    }
+
+    public double getTotalTaxableIncome() {
+        return totalTaxableIncome;
+    }
+
+    public void setTotalTaxableIncome(double totalTaxableIncome) {
+        this.totalTaxableIncome = totalTaxableIncome;
+    }
+
+    public double getTotalTaxPayed() {
+        return totalTaxPayed;
+    }
+
+    public void setTotalTaxPayed(double totalTaxPayed) {
+        this.totalTaxPayed = totalTaxPayed;
+    }
 }
